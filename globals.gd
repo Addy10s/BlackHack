d8963583a92ff529c8dee4ea04ffec1a9cdc9223
Addy Tennies <addy@tennies.us>
2026-07-13 21:49:27 -0500

    fixing defaults

--- globals.gd
+++ globals.gd
@@ -5,7 +5,7 @@ enum SUCCESS_t { FAIL, SUCCESS, PERFECT }
 var success
 @warning_ignore("shadowed_global_identifier")
 var round = 0
-var max_power = 2100
+var max_power = 21
 var lives = 3
-var tokens = 100
+var tokens = 10
 var deck = Deck_t.new()
